4d0e20ff158ca5abf82ba2a5424499fe9abe8f47
Wedwin02 <navarretee210@gmail.com>
2020-10-07 00:37:02 -0600

    +Pantallas Listas

--- app/src/main/java/com/uso/guia5/MainActivity.java
+++ app/src/main/java/com/uso/guia5/MainActivity.java
@@ -1,14 +1,64 @@
 package com.uso.guia5;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.ButtonBarLayout;
 
+import android.content.Intent;
+import android.content.pm.ActivityInfo;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
-
+ private Button btnDatos;
+ private Button btnCalculadora;
+ private Button btnLogin;
+ private Button btnConversacion;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
+
+        this.btnCalculadora = findViewById(R.id.btnCalculadora);
+        this.btnDatos = findViewById(R.id.btnDatos);
+        this.btnLogin = findViewById(R.id.btnLogin);
+        this.btnConversacion = findViewById(R.id.btnConversacion);
+
+        this.btnCalculadora.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(MainActivity.this,Calculadora.class);
+                startActivity(i);
+            }
+        });
+
+        this.btnDatos.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(MainActivity.this,Datos.class);
+                startActivity(i);
+
+            }
+        });
+        this.btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(MainActivity.this,Login.class);
+                startActivity(i);
+            }
+        });
+        this.btnConversacion.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(MainActivity.this,Conversacion.class);
+                startActivity(i);
+
+            }
+        });
+
+
+
+
     }
 }
